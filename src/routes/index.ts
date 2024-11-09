92c01b728d4ccc086448926be1f5703428412991
Jeonguk Choi <91456421+cjeonguk@users.noreply.github.com>
2024-11-09 13:51:08 +0900

    Add index router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,22 @@
-import express from 'express';
-import type { Request, Response } from 'express';
-const router = express.Router();
+import express from "express"
+import type { Request, Response } from "express"
 
-router.get('/', (req: Request, res: Response) => {
-    res.send('Hello, World!');
-});
+const router = express.Router()
 
-export default router;
+router.get("/", (req: Request, res: Response) => {
+  res.send("Hello, World!")
+})
+
+router.post("/", (req: Request, res: Response) => {
+  res.send("Hello, World!")
+})
+
+router.put("/", (req: Request, res: Response) => {
+  res.send("Hello, World!")
+})
+
+router.delete("/", (req: Request, res: Response) => {
+  res.send("Hello, World!")
+})
+
+export default router
