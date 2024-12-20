ffd9c7253b4fc00ebdf75e68e05bb1f7232835b3
Jeonguk Choi <91456421+cjeonguk@users.noreply.github.com>
2024-12-20 11:14:47 +0900

    Add project controller

---
+++ src/routes/project.ts
@@ -0,0 +1,14 @@
+import express from "express"
+import {
+  getProjects,
+  createProject,
+  updateProject,
+  deleteProject,
+} from "../controllers/project"
+
+const router = express.Router()
+
+router.route("/:id").get(getProjects).put(updateProject).delete(deleteProject)
+
+router.post("/", createProject)
+export default router
