5300c0030f892f285f9c60d6f6834fabcefcb4a4
Seohyun Yun <yunseohyun2008@gmail.com>
2024-12-26 19:37:40 +0900

    docs: about에서 스키마 관련 코드 삭제

--- src/models/about.ts
+++ src/models/about.ts
@@ -1,10 +1,8 @@
 import mongoose from "mongoose"
-import { projectSchema } from "./project"
 
 // 부서 소개 스키마
 const aboutSchema = new mongoose.Schema({
-  introduction: { type: String, required: true },
-  projects: [projectSchema],
+  introduction: { type: String, required: true }
 })
 
 export default mongoose.model("About", aboutSchema)
