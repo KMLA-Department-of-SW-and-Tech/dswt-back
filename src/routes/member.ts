da61a815d2c87001e6a8897e43b366165667755f
Seohyun Yun <yunseohyun2008@gmail.com>
2024-12-28 14:25:59 +0900

    feat: routes/member 만듦

--- src/routes/member.ts
+++ src/routes/member.ts
@@ -4,4 +4,16 @@ import {
     createMember,
     putMember,
     deleteMember
-}
+} from "../controllers/member"
+
+const router = express.Router()
+
+router.route("/:id").get(getMember).put(updateMember).delete(deleteMember)
+router
+    .route("/:id/detail")
+    .get(getMember)
+    .put(updateMember)
+    .delete(deleteMember)
+
+export default router
+
